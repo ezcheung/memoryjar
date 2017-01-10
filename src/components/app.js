c2b7bf8e255be4aaf78711fc6389859f1685f85e
Elliot Cheung <elliotccheung@gmail.com>
2017-01-10 15:00:49 -0600

    Moved main component to separate app file

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {
+  Text,
+  View,
+  TouchableHighLight,
+  TextInput,
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    )
+  }
+}

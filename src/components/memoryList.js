2fe3a3a68db548f9b8d8a4cf6d00ef3fd944b1d6
Elliot Cheung <elliotccheung@gmail.com>
2017-01-10 16:23:29 -0600

    Wrote willMount function to fetch memories

--- src/components/memoryList.js
+++ src/components/memoryList.js
@@ -3,19 +3,31 @@ import React from 'react';
 import {
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  AsyncStorage,
 } from 'react-native';
 
 import Memory from './memory';
 
 export default class MemoryList extends React.Component() {
-  
+
   constructor() {
     super();
     this.state = {
       fetching : true,
       memories : []
     }
+  }
+
+  componentWillMount() {
+    AsyncStorage.getItem('memories')
+    .then(memories => {
+      this.setState({fetching : false, memories: memories});
+    })
+    .catch(err => {
+      console.error(err);
+      this.props.toggleView();
+    })
   }
 
   render() {
